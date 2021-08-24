028b200173b161b52c0119a7a8db94d76fcb0b77
Aditya Aluru <alurua@Adityas-MacBook-Pro.local>
2021-08-24 13:27:00 +0530

    Updated with remote api calls

---
+++ src/services/genreService.js
@@ -0,0 +1,7 @@
+import { toast } from "react-toastify";
+import http from "./httpService";
+
+export async function getGenres() {
+  const genres = await http.get(process.env.REACT_APP_REMOTE_URL + "/api/genres");
+  return genres.data;
+}
